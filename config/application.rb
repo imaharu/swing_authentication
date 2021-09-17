70a34d272d4388631ade853fac1578d4983541ca
chihaso <cycyccq@gmail.com>
2021-09-17 21:14:00 +0900

    home_controller追加

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module SwingAuthentication
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
