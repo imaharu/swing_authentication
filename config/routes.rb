3bc26b5a0c7bcabd9bac23a08d62324ee66a0f71
imaharu <light1200out@gmail.com>
2021-09-20 21:50:44 +0900

    SignUpの際に、nicknameを登録する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index' 
-  devise_for :users
+  root 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
