dfcb40c8de96cb112928897aca4fd9711d87a682
imaharu <light1200out@gmail.com>
2021-09-20 22:00:17 +0900

    deviseの指示に従い、リファクタリング

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +9,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -39,15 +39,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-
-  def sign_up_params
-    super.merge(nickname: params[:user][:nickname])
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
