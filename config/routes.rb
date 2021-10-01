dd61269501bee603504245b080907722281eab3a
chihaso <cycyccq@gmail.com>
2021-10-01 19:56:19 +0900

    sessions_controllerとルーティング追加
    - まだnew_session_pathがないというエラーが出ている

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :unconfirmed_users, class_name: 'UnconfirmedUser', controllers: {
     confirmations: 'user/confirmations',
   }
+  devise_for :password_authentications, class_name: 'User::PasswordAuthentication', controllers: {
+    sessions: 'user/sessions',
+  }
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
